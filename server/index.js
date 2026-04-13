56bdff84f1638a66257f9f635ae6b5faa164567e
Priyadarshi <priyadarshikashyap176@gmail.com>
2026-04-13 10:54:18 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -171,6 +171,10 @@ app.post("/api/analyze", async (req, res) => {
 });
 
 // ===============================
-app.listen(5000, () => {
-  console.log("🚀 Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.get("/", (req, res) => {
+  res.send("Backend is running 🚀");
+});
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on port ${PORT}`);
 });
